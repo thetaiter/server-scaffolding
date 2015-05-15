b91caec0d76d2a0ee74cc5ca558146caeaa997df
Ian Tait <thetaiter@gmail.com>
2015-05-15 11:26:32 -0500

    Updated start and stop scripts

--- start.sh
+++ start.sh
@@ -2,30 +2,53 @@
 
 RUN_SCRIPT='node server.js'
 LOG_LOCATION='log'
-KILLED=false
+PORT=3000
+
+if [ $# -eq 0 ]
+then
+    printf "\nNo port specified Using default port (%s).\n" $PORT
+elif [ "$#" -eq 2 -a \( "$1" = "-p" -o "$1" = "--port" \) ]
+then
+    PORT=$2
+else
+    printf "\nUsage:\n\n    Optional Arguments:\n\t-p, --port\tSpecify port number\n\n"
+    exit 1
+fi
+    
 
 cd $(dirname $0)
 
-if ! [ -d "$LOG_LOCATION" ]; then
+if ! [ -d "$LOG_LOCATION" ]
+then
     mkdir -p $LOG_LOCATION
 fi
 
-if [ -f $LOG_LOCATION/.pid ]; then
+if [ -f $LOG_LOCATION/.pid ]
+then
     PID=$(cat $LOG_LOCATION/.pid)
 
-    printf "\nThere is already a server running with PID=%i.\nWould you like to kill this process before running a new process?\nAll logging information for the current process will be lost regardless. (Y/N): " $PID
+    printf "\nAn instance of your server is already running (PID=%s)." $PID
+    printf "\n\nWhat would you like to do?\n"
+    printf "\n\tKill   - Attempt to kill the currently running server before starting a new instance"
+    printf "\n\tIgnore - Ignore the currently running server and start a new instance on a different port"
+    printf "\n\tAbort  - Do nothing"
+    printf "\n\nAll information for the currently running server will be lost if you do not abort."
+    printf "\n\n(Kill/Ignore/Abort) [Abort]: "
 
     read answer
 
-    if [ "$answer" = "Y" ] || [ "$answer" = "y" ] || [ "$answer" = "yes" ] || [ "$answer" = "Yes" ]; then
+    if [ "$answer" = "K" ] || [ "$answer" = "k" ] || [ "$answer" = "Kill" ] || [ "$answer" = "kill" ]
+    then
         ./stop.sh
-        KILLED=true
-    else
+    elif [ "$answer" = "I" ] || [ "$answer" = "i" ] || [ "$answer" = "Ignore" ] || [ "$answer" = "ignore" ]
+    then
         printf "\nIgnoring currently running server process.\n"
+        printf "\nWhat port would you like to run the new instance on? []"
+    else
+        printf "\nAborting.\n\n"
+        exit 3
     fi
-fi
-
-if [ "$KILLED" = false ]; then
+else
     echo
 fi
 
@@ -35,4 +58,4 @@ nohup $RUN_SCRIPT 2> $LOG_LOCATION/error.log > $LOG_LOCATION/server.log &
 PID=$!
 echo $PID > $LOG_LOCATION/.pid
 
-printf "Process started, PID = %i.\nLogging stdout and stderr to files in '%s' folder.\n\n" $PID $LOG_LOCATION
+printf "Server started, PID = %s.\nLogging stdout and stderr to files in '%s' folder.\n\n" $PID $LOG_LOCATION
--- stop.sh
+++ stop.sh
@@ -8,7 +8,7 @@ if [ -f $LOG_LOCATION/.pid ]; then
     PID=$(cat $LOG_LOCATION/.pid)
     printf "\nAttempting to kill running server process PID=%i...\n" $PID
     
-    if kill $PID > /dev/null 2>&1; then
+    if kill -9 $PID > /dev/null 2>&1; then
         printf "Process PID=%i killed.\n\n" $PID
     else
         printf "Process PID=%i does not exist. Perhaps it has already died?\n\n" $PID
