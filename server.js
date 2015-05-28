ad5aa1912745a2a5fb858978943dba6c7d4475da
Ian Tait <thetaiter@gmail.com>
2015-05-27 22:01:12 -0500

    Finished implementation of ignore option

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ var server = app.listen(default_port, function() {
     var host = server.address().address;
     var port = server.address().port;
 
+    if (host === "::") host = "localhost"
+
     console.log('\nTest NodeJS Express server listening at http://%s:%s\n', host, port);
 });
 
 server.on('error', function(err) {
-    if (err.message === 'listen EADDRINUSE')
-    console.error('\nPort %s is in use. Perhaps you already have a server running on this port?\n', default_port);
+    if (err.message === 'listen EADDRINUSE') {
+        console.error('\nPort %s is in use. Perhaps you already have a server running on this port?\n', default_port);
+    }
 });
--- start.sh
+++ start.sh
@@ -4,14 +4,12 @@ RUN_SCRIPT='node server.js'
 LOG_LOCATION='log'
 PORT=3000
 
-if [ $# -eq 0 ]
-then
-    printf "\nNo port specified Using default port (%s).\n" $PORT
-elif [ "$#" -eq 2 -a \( "$1" = "-p" -o "$1" = "--port" \) ]
+if [ "$#" -eq 2 -a \( "$1" = "-p" -o "$1" = "--port" \) ]
 then
     PORT=$2
-else
-    printf "\nUsage:\n\n    Optional Arguments:\n\t-p, --port\tSpecify port number\n\n"
+elif [ ! "$#" -eq 0 ]
+then
+    printf "\nUsage: ./start.sh <Optional Arguments>\n\n    Optional Arguments:\n\t-p, --port\tSpecify port number\n\n"
     exit 1
 fi
     
@@ -27,12 +25,17 @@ if [ -f $LOG_LOCATION/.pid ]
 then
     PID=$(cat $LOG_LOCATION/.pid)
 
-    printf "\nAn instance of your server is already running (PID = %s)." $PID
-    printf "\n\nWhat would you like to do?\n"
+    if [ -f $LOG_LOCATION/.port ]
+    then
+        PORT=$(cat $LOG_LOCATION/.port)
+    fi
+
+    printf "\nAn instance of your server is already running (PORT = %s, PID = %s)." $PORT $PID
+    printf "\nWhat would you like to do?\n"
     printf "\n\tKill   - Attempt to kill the currently running server before starting a new instance"
     printf "\n\tIgnore - Ignore the currently running server and start a new instance on a different port"
     printf "\n\tAbort  - Do nothing"
-    printf "\n\nAll information for the currently running server will be lost if you do not abort."
+    printf "\n\nWARNING: All stdout and stderr info for the currently running server will be lost if you do not abort."
     printf "\n\n(Kill/Ignore/Abort) [Abort]: "
 
     read answer
@@ -43,13 +46,17 @@ then
     elif [ "$answer" = "I" ] || [ "$answer" = "i" ] || [ "$answer" = "Ignore" ] || [ "$answer" = "ignore" ]
     then
         printf "\nIgnoring currently running server process.\n"
-        printf "\nWhat port would you like to run the new instance on? [%s +1]: " $PORT
+        printf "\nWhat port would you like to run the new instance on? [$(($PORT+1))]: "
 
         read port
 
         if [ ! -z "$port" ]
         then
-            if [[ "$port" =~ ^[0-9]+$ ]]
+            is_all_digits () {
+                case $1 in *[!0-9]*) false;; esac
+            }
+
+            if is_all_digits $port
             then
                 printf "\nIgnoring currently running server process."
                 printf "\nSetting PORT=%s...\n\n" $port
@@ -61,7 +68,7 @@ then
                 exit 2
             fi
         else
-            ((PORT++))
+            PORT=$(($PORT+1))
             printf "\nSetting PORT=%s\n\n" $PORT
         fi
     else
@@ -77,5 +84,6 @@ printf "Attempting to run '%s'...\n" "$RUN_SCRIPT"
 PORT="$PORT" nohup $RUN_SCRIPT 2> $LOG_LOCATION/error.log > $LOG_LOCATION/server.log &
 PID=$!
 echo $PID > $LOG_LOCATION/.pid
+echo $PORT > $LOG_LOCATION/.port
 
-printf "Server started, PID = %s.\nLogging stdout and stderr to files in '%s' folder.\n\n" $PID $LOG_LOCATION
+printf "Server started on PORT = %s, PID = %s.\nLogging stdout and stderr to files in '%s' folder.\n\n" $PORT $PID $LOG_LOCATION
