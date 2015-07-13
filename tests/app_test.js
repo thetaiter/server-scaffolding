2858371f22befe48f0f09bbbcf6dd5210d23f195
Ian Tait <thetaiter@gmail.com>
2015-07-12 21:41:06 -0500

    Added test for DELETE /users/<test-user-id>/edit

--- tests/app_test.js
+++ tests/app_test.js
@@ -24,7 +24,7 @@ describe('Home', function() {
 describe('Users', function() {
     this.timeout(5000);
 
-    it('should repsond with with 200', function(done) {
+    it('GET /users should repsond with json and 200', function(done) {
         api.get('/users')
             .set('Accept', 'application/json')
             .expect(200)
@@ -37,7 +37,7 @@ describe('Users', function() {
             });
     });
 
-    it('should respond with 200', function(done) {
+    it('GET /users/new should respond with json and 200', function(done) {
         api.get('/users/new')
             .set('Accept', 'application/json')
             .expect(200)
@@ -50,7 +50,9 @@ describe('Users', function() {
             });
     });
 
-    it('should post a user to the database and respond with 302, redirecting to /users', function(done) {
+    var db_user;
+
+    it('POST /users should post a user to the database and respond with 302', function(done) {
         api.post('/users')
             .send({
                 name: 'Test User',
@@ -77,8 +79,30 @@ describe('Users', function() {
 
                         assert.equal(isMatch, true);
 
+                        db_user = user;
+
                         return done();
                     });
+                });
+            });
+    });
+
+    it('DELETE /users/<test-user-id>/edit should delete the user from the database and respond with 302', function(done) {
+        api.delete('/users/' + db_user._id + '/edit')
+            .expect(302)
+            .end(function(err, res) {
+                if (err) {
+                    return done(err);
+                }
+
+                mongoose.model('User').findOne({username: 'testuser1'}, function(err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    assert.isNull(user);
+
+                    return done();
                 });
             });
     });
